ffa32bc1869e3b30e05fe5e702fee8ded88bde16
LeviMatias <matilevi35@gmail.com>
2024-12-04 12:31:02 -0300

    setup

---
+++ src/test/java/com/upp/UppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
