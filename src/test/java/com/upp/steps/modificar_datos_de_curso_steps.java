8a8bf44ca9e9a3c558a88e4d5a3f6ee01dad16e8
LeviMatias <matilevi35@gmail.com>
2025-10-02 21:10:43 -0300

    modificar datos de curso

---
+++ src/test/java/com/upp/steps/modificar_datos_de_curso_steps.java
@@ -0,0 +1,77 @@
+package com.upp.steps;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.upp.dto.CursoDTO;
+import com.upp.steps.shared.TokenHolder;
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Entonces;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class modificar_datos_de_curso_steps {
+  @Autowired private WebTestClient webTestClient;
+  @Autowired private TokenHolder tokenHolder;
+
+  private FluxExchangeResult<CursoDTO> result;
+
+  @Cuando("se modifica el curso con código {string}, máximo de alumnos {int} y materia {string}")
+  public void seModificaElCurso(String codigo, Integer maximoAlumnos, String codigoMateria) {
+    CursoDTO cursoEnviado = new CursoDTO(codigo, maximoAlumnos, codigoMateria);
+
+    this.result =
+        webTestClient
+            .put()
+            .uri("/api/cursos/{codigo}", codigo)
+            .header("Authorization", "Bearer " + tokenHolder.getToken())
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(cursoEnviado)
+            .exchange()
+            .returnResult(CursoDTO.class);
+  }
+
+  @Entonces("se actualiza la información del curso {string} exitosamente")
+  public void seActualizaLaInformacionDelCursoExitosamente(String codigo) {
+    assertEquals(HttpStatus.OK, result.getStatus());
+
+    var resultGetCurso =
+        webTestClient
+            .get()
+            .uri("/api/cursos/{codigo}", codigo)
+            .header("Authorization", "Bearer " + tokenHolder.getToken())
+            .exchange()
+            .returnResult(CursoDTO.class);
+
+    assertEquals(HttpStatus.OK, resultGetCurso.getStatus());
+  }
+
+  @Entonces("el curso {string} tiene máximo de alumnos {int} y materia {string}")
+  public void elCursoTieneLosDatosEsperados(String codigo, Integer maximoAlumnos, String codigoMateria) {
+    var response =
+        webTestClient
+            .get()
+            .uri("/api/cursos/{codigo}", codigo)
+            .header("Authorization", "Bearer " + tokenHolder.getToken())
+            .exchange()
+            .returnResult(CursoDTO.class);
+
+    assertEquals(HttpStatus.OK, response.getStatus());
+
+    CursoDTO curso = response.getResponseBody().blockFirst();
+    assertNotNull(curso);
+    assertEquals(codigo, curso.getCodigo());
+    assertEquals(maximoAlumnos, curso.getMaximoDeAlumnos());
+    assertEquals(codigoMateria, curso.getCodigoMateria());
+  }
+
+  @Entonces("no se actualiza la información del curso exitosamente")
+  public void noSeActualizaLaInformacionDelCursoExitosamente() {
+    assertEquals(HttpStatus.NOT_FOUND, result.getStatus());
+  }
+}
--- src/test/resources/features/secretaria_de_planificacion/modificar_datos_de_curso.feature
+++ src/test/resources/features/secretaria_de_planificacion/modificar_datos_de_curso.feature
@@ -1,10 +1,33 @@
 #language: es
-@todo
-Característica: Modificar curso
+Característica: Modificar datos de un curso
+  Antecedentes:
+    Dado que hay un gestor academico logueado
+    Y que existe una materia con el código de materia "125-M" y nombre "Cálculo I"
+    Y que existe una materia con el código de materia "126-M" y nombre "Física I"
+    Y que hay un gestor de planificacion logueado
+    Y se registra un nuevo curso con código "CURSO-MOD-01", máximo de alumnos 25 y materia "125-M"
+    Y se registra un nuevo curso con código "CURSO-MOD-02", máximo de alumnos 30 y materia "126-M"
 
-  Escenario: : Modificar curso es exitoso
-    Dado que existe un responsable de Secretaría de Planificación
-    Y existe un curso con "codigo" con "cod. curso", "docentes a cargo", "cant max de alumnos"
-    Cuando cuando se modifica el curso con "codigo" con "cod. curso", "docentes a cargo", "cant max de alumnos"
-    Entonces se actualiza la informacion del curso
 
+  Escenario: Modificar máximo de alumnos de un curso es exitoso
+    Cuando se modifica el curso con código "CURSO-MOD-01", máximo de alumnos 35 y materia "125-M"
+    Entonces se actualiza la información del curso "CURSO-MOD-01" exitosamente
+    Y el curso "CURSO-MOD-01" tiene máximo de alumnos 35 y materia "125-M"
+
+  Escenario: Modificar materia de un curso es exitoso
+    Cuando se modifica el curso con código "CURSO-MOD-02", máximo de alumnos 30 y materia "126-M"
+    Entonces se actualiza la información del curso "CURSO-MOD-02" exitosamente
+    Y el curso "CURSO-MOD-02" tiene máximo de alumnos 30 y materia "126-M"
+
+  Escenario: Modificar todos los datos de un curso es exitoso
+    Cuando se modifica el curso con código "CURSO-MOD-01", máximo de alumnos 40 y materia "126-M"
+    Entonces se actualiza la información del curso "CURSO-MOD-01" exitosamente
+    Y el curso "CURSO-MOD-01" tiene máximo de alumnos 40 y materia "126-M"
+
+  Escenario: Modificar curso inexistente fracasa
+    Cuando se modifica el curso con código "CURSO-INEXISTENTE", máximo de alumnos 20 y materia "125-M"
+    Entonces no se actualiza la información del curso exitosamente
+
+  Escenario: Modificar curso con materia inexistente fracasa
+    Cuando se modifica el curso con código "CURSO-MOD-01", máximo de alumnos 25 y materia "MATERIA-INEXISTENTE"
+    Entonces no se actualiza la información del curso exitosamente
