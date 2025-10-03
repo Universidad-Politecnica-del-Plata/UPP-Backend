e28d2dbeee9df9994e66e7f84d01fc13d988821d
LeviMatias <matilevi35@gmail.com>
2025-10-02 21:44:50 -0300

    fix roles

--- src/main/java/com/upp/controller/MateriaController.java
+++ src/main/java/com/upp/controller/MateriaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/materias")
-@PreAuthorize("hasRole('GESTION_ACADEMICA')")
+
 public class MateriaController {
   private final MateriaService materiaService;
 
@@ -21,12 +21,14 @@ public class MateriaController {
   }
 
   @PostMapping
+  @PreAuthorize("hasRole('GESTION_ACADEMICA')")
   public ResponseEntity<?> crearMateria(@Valid @RequestBody MateriaDTO materiaDTO) {
     MateriaDTO resultado = materiaService.crearMateria(materiaDTO);
     return ResponseEntity.status(HttpStatus.CREATED).body(resultado);
   }
 
   @PutMapping("/{codigo}")
+  @PreAuthorize("hasRole('GESTION_ACADEMICA')")
   public ResponseEntity<?> modificarMateria(
       @PathVariable String codigo, @RequestBody MateriaDTO materiaDTO) {
     MateriaDTO resultado = materiaService.modificarMateria(codigo, materiaDTO);
@@ -34,6 +36,7 @@ public class MateriaController {
   }
 
   @DeleteMapping("/{codigo}")
+  @PreAuthorize("hasRole('GESTION_ACADEMICA')")
   public ResponseEntity<?> eliminarMateria(@PathVariable String codigo) {
     materiaService.eliminarMateria(codigo);
     return ResponseEntity.status(HttpStatus.OK)
@@ -41,12 +44,14 @@ public class MateriaController {
   }
 
   @GetMapping("/{codigo}")
+  @PreAuthorize("hasRole('GESTION_ACADEMICA')")
   public ResponseEntity<?> obtenerMateriaPorCodigo(@PathVariable String codigo) {
     MateriaDTO materia = materiaService.obtenerMateriaPorCodigo(codigo);
     return ResponseEntity.status(HttpStatus.OK).body(materia);
   }
 
   @GetMapping
+  @PreAuthorize("hasRole('GESTION_ACADEMICA') or hasRole('GESTOR_DE_PLANIFICACION')")
   public ResponseEntity<List<MateriaDTO>> obtenerTodasLasMaterias() {
     List<MateriaDTO> materias = materiaService.obtenerTodasLasMaterias();
 
